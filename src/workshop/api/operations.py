fd4dd5ccf901f029e82c88cebadabb95c1989def
mbgoodguy <nerooren4001@gmail.com>
2023-11-02 11:44:28 +0300

    Add operations filter by kind

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from ..models.operations import Operation
+from ..models.operations import Operation, OperationKind
 from ..services.operations import OperationService
 
 router = APIRouter(
@@ -11,5 +11,5 @@ router = APIRouter(
 
 # благодаря указания response_model pydantic автоматом преобразует список в модели pydantic и с помощью них на выходе получит JSON
 @router.get('/', response_model=List[Operation])
-def get_operations(service: OperationService = Depends()):
-    return service.get_list()
+def get_operations(kind: OperationKind = None, service: OperationService = Depends()):
+    return service.get_list(kind=kind)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -4,16 +4,16 @@ from fastapi import Depends
 
 from workshop import tables
 from workshop.database import get_session, Session
+from workshop.models.operations import OperationKind
 
 
 class OperationService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self) -> List[tables.Operation]:
-        operations = (
-            self.session
-            .query(tables.Operation)
-            .all()
-        )
+    def get_list(self, kind: OperationKind) -> List[tables.Operation]:
+        query = self.session.query(tables.Operation)
+        if kind:
+            query = query.filter_by(kind=kind)
+        operations = query.all()
         return operations
