bab2cf5ed7de615f8db380b30c6be0e8b25458e0
mbgoodguy <nerooren4001@gmail.com>
2023-11-05 22:14:12 +0300

    Add put and delete routers

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,8 +1,9 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
+from starlette.responses import Response
 
-from ..models.operations import Operation, OperationKind, OperationCreate
+from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
 from ..services.operations import OperationService
 
 router = APIRouter(
@@ -21,7 +22,7 @@ def create_operations(
         operation_data: OperationCreate,
         service: OperationService = Depends()
 ):
-    return service.add_operation(operation_data)
+    return service.add_operations(operation_data)
 
 
 @router.get('/{user_id}', response_model=Operation)
@@ -30,3 +31,21 @@ def get_operation_by_id(
         service: OperationService = Depends()
 ):
     return service.get_operation(operation_id)
+
+
+@router.put('/{operation_id}', response_model=Operation)
+def update_operation_by_id(
+        operation_id: int,
+        operation_data: OperationUpdate,
+        service: OperationService = Depends(),
+):
+    return service.update_operation(operation_id, operation_data)
+
+
+@router.delete('/{operation_id}', response_model=Operation)
+def delete_operation_by_id(
+        operation_id: int,
+        service: OperationService = Depends(),
+):
+    service.delete_operation(operation_id)
+    return Response(status_code=204)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -5,12 +5,23 @@ from starlette import status
 
 from workshop import tables
 from workshop.database import get_session, Session
-from workshop.models.operations import OperationKind, OperationCreate
+from workshop.models.operations import OperationKind, OperationCreate, OperationUpdate
 
 
 class OperationService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
+
+    def _get(self, operation_id: int) -> tables.Operation:
+        operation = (
+            self.session
+            .query(tables.Operation)
+            .filter_by(id=operation_id)
+            .first()
+        )
+        if not operation:
+            raise HTTPException(status_code=404, detail=f'No operation with id {operation_id}')
+        return operation
 
     def get_operations_list(self, kind: OperationKind) -> List[tables.Operation]:
         query = self.session.query(tables.Operation)
@@ -26,13 +37,18 @@ class OperationService:
         return operation
 
     def get_operation(self, operation_id: int) -> tables.Operation:
-        operation = (
-            self.session
-            .query(tables.Operation)
-            .filter_by(id=operation_id)
-            .first()
-        )
-        if not operation:
-            raise HTTPException(status_code=404, detail=f'No operation with id {operation_id}')
+        return self._get(operation_id)
+
+    def update_operation(self, operation_id: int, operation_data: OperationUpdate) -> tables.Operation:
+        operation = self._get(operation_id)
+
+        for field, value in operation_data:
+            setattr(operation, field, value)
+        self.session.commit()
         return operation
-r
+
+    def delete_operation(self, operation_id: int):
+        operation = self._get(operation_id)
+
+        self.session.delete(operation)
+        self.session.commit()
