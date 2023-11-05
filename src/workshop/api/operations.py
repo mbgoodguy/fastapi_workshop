98e652ec7680588722e6a8e06162742030401431
mbgoodguy <nerooren4001@gmail.com>
2023-11-05 19:19:22 +0300

    Add post ednpoint for operations

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,7 +1,10 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from ..models.operations import Operation, OperationKind
+
+from .. import tables
+from ..database import Session, get_session
+from ..models.operations import Operation, OperationKind, OperationCreate
 from ..services.operations import OperationService
 
 router = APIRouter(
@@ -12,4 +15,12 @@ router = APIRouter(
 # благодаря указания response_model pydantic автоматом преобразует список в модели pydantic и с помощью них на выходе получит JSON
 @router.get('/', response_model=List[Operation])
 def get_operations(kind: OperationKind = None, service: OperationService = Depends()):
-    return service.get_list(kind=kind)
+    return service.get_operations_list(kind=kind)
+
+
+@router.post('/', response_model=OperationCreate)
+def create_operations(
+        operation_data: OperationCreate,
+        service: OperationService = Depends()
+):
+    return service.add_operations(operation_data)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -4,16 +4,22 @@ from fastapi import Depends
 
 from workshop import tables
 from workshop.database import get_session, Session
-from workshop.models.operations import OperationKind
+from workshop.models.operations import OperationKind, Operation, OperationCreate
 
 
 class OperationService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self, kind: OperationKind) -> List[tables.Operation]:
+    def get_operations_list(self, kind: OperationKind) -> List[tables.Operation]:
         query = self.session.query(tables.Operation)
         if kind:
             query = query.filter_by(kind=kind)
         operations = query.all()
         return operations
+
+    def add_operations(self, operation_data: OperationCreate) -> tables.Operation:
+        operation = tables.Operation(**operation_data.model_dump())
+        self.session.add(operation)
+        self.session.commit()
+        return operation
